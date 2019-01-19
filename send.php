301e863aec8f9c07e8973f194279cb4fc8287385
tsybinn <38625057+tsybinn@users.noreply.github.com>
2019-01-19 12:42:17 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,21 @@
+<?
+if((isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'tsubinn@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+		
+		header("Location: form.html"); /* Перенаправление браузера */
+        $headers .= "From: заявка на подключение <taxi@taxi999.ru>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
